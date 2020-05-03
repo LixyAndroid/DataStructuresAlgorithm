37406fda0ec9965607b55963d01b705197cc6008
xuyangli <958607524@qq.com>
2020-05-03 21:52:41 +0800

    插入排序

--- src/xuyang/datadtructuresalgorithm/sort/InsertSort.java
+++ src/xuyang/datadtructuresalgorithm/sort/InsertSort.java
@@ -54,13 +54,12 @@ public class InsertSort {
         for (int i = 1; i < arr.length; i++) {
             //定义待插入的数
              insertVal = arr[i];
-             insertIndex = i - 1;//即arr[1]的前面这个数的下标
+             insertIndex = i - 1;//即arr[i]的前面这个数的下标
             //1,insertIndex>=0保证再给insertVal找插入位置，不越界
             //2，insertVal < arr[insertIndex]待插入的数还没有找到插入位置
             //，3，需要将arr[insertIndex]后移
             //也可以从大到小
             while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
-
 
                 arr[insertIndex + 1] = arr[insertIndex];
                 insertIndex--;
